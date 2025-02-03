a501aebca5b287a1acafb2df95f7478bf300a786
Milos Tasic <tasic29@gmail.com>
2025-02-03 14:06:27 +0100

    creted vue project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <main>
+    <header>
+      <img src="./assets/pinia-logo.svg" alt="pinia logo" />
+      <h1>Pinia Tasks</h1>
+    </header>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
