2b7cd389740448b1621ab8d4aade779fb35210c2
Milos Tasic <tasic29@gmail.com>
2025-02-03 14:49:18 +0100

    added TaskDetail component

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,26 @@
       <img src="./assets/pinia-logo.svg" alt="pinia logo" />
       <h1>Pinia Tasks</h1>
     </header>
+
+    <div class="task-list">
+      <div v-for="task in taskStore.tasks" :key="task.id">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
   </main>
 </template>
 
 <script>
-export default {};
+import TaskDetails from "./components/TaskDetails.vue";
+import { useTaskStore } from "./stores/TaskStore";
+export default {
+  components: { TaskDetails },
+  setup() {
+    const taskStore = useTaskStore();
+
+    return {
+      taskStore,
+    };
+  },
+};
 </script>
--- src/main.js
+++ src/main.js
@@ -4,4 +4,4 @@ import { createPinia } from "pinia";
 import { createApp } from "vue";
 import App from "./App.vue";
 
-createApp(App).use(createPinia).mount("#app");
+createApp(App).use(createPinia()).mount("#app");
